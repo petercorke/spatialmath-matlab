bced0366a643f9e3a6cdcc3e25a42d94ace919c0
Peter Corke <peter.i.corke@gmail.com>
2018-12-16 17:52:17 +1000

    Add code to automatically compute the phase for best viewing

--- plot_ribbon.m
+++ plot_ribbon.m
@@ -1,7 +1,27 @@
 clf
 trplot( SE3 )
+shading interp
 
-plot_ribbon2( SE3 )
+Tx = SE3(0.7,0,0)*SE3.Ry(pi/2);
+Ty = SE3(0, 0.7,0)*SE3.Rx(pi/2);
+Tz = SE3(0, 0, 0.7);
+
+c = circle([0 0 0], 0.25);
+
+p = get(gca, 'CameraPosition')';
+
+[~,kx] = min( colnorm( Tx*c-p ) );
+[~,ky] = min( colnorm( Ty*c-p ) );
+[~,kz] = min( colnorm( Tz*c-p ) );
+[kx ky kz]
+
+plot_ribbon2( Tx, kx )
+plot_ribbon2( Ty, ky )
+plot_ribbon2( Tz, kz )
+
+
+
+camlight
 
 
 %   ^
@@ -9,18 +29,18 @@ plot_ribbon2( SE3 )
 %   |  ^  +----------------------|  \
 %   |  |  |                          .
 %   |  v  +----------------------|  /
-%   v  w1                        | /
-%   w2    <---------- d --------->
-%         <--------- wrap ----------->
-function plot_ribbon2(pose)
-    radius = 0.5;
+%   |  w1                        | /
+%   v     <---------- d --------->
+%   w2    <--------- wrap ----------->
+function plot_ribbon2(pose, ph)
+    radius = 0.25;
     d = 0.90;
     w1 = 0.2;
     w2 = 0.4;
     N = 100;
     Nd = floor(N*d);
     wrap = 0.8;
-    phase = -.3;
+    phase = (ph - 5) / 50 * 2*pi
     
     % compute canonic arrow about the z-axis and centered at origin
     
@@ -48,7 +68,10 @@ function plot_ribbon2(pose)
     C = cat(3, z1*1, z1*0, z1*0);
     hg = hgtransform
     hold on
-    surf(X, Y, Z, C, 'Parent', hg, 'EdgeColor', 'None', 'FaceColor', [1 0 0])
+    surf(X, Y, Z, C, 'Parent', hg, 'EdgeColor', 'None', 'FaceColor', [1 0 0], ...
+        'BackFaceLighting', 'reverselit', 'SpecularStrength', 0.2, 'DiffuseStrength', 0.8)
+    
+    % 0.9, 0.6
     hold off
     
     hg.Matrix = pose.T;
