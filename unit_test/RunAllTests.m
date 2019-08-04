c925be4f8d46f819db1e677cf0129b418e5d5b64
Peter Corke <peter.i.corke@gmail.com>
2019-08-04 14:53:20 +1000

    add path to toolbox add some comments

--- unit_test/RunAllTests.m
+++ unit_test/RunAllTests.m
@@ -1,3 +1,5 @@
+% run the tests for Travis CI
+
 import matlab.unittest.plugins.CodeCoveragePlugin
 import matlab.unittest.plugins.codecoverage.CoberturaFormat
 import matlab.unittest.TestRunner
@@ -5,18 +7,19 @@ import matlab.unittest.TestRunner
 suite = testsuite('IncludeSubfolders', false);
 runner = TestRunner.withTextOutput;
 
-% do a coverage report
+% add a coverage report
 reportFile = fullfile('..', 'coverage.xml');
 reportFormat = CoberturaFormat(reportFile);
 plugin = CodeCoveragePlugin.forFolder('..', 'Producing',reportFormat);
 runner.addPlugin(plugin);
+
+%% setup the path
+addpath ..
+
+%% Run all unit tests in my repository
 
 % Run all unit tests in my repository.
 results = runner.run(suite);
 
 % Assert no tests failed.
 assert(all(~[results.Failed]));
-
-% build an installable toolbox file
-%cd ..
-%matlab.addons.toolbox.packageToolbox('ToolboxPackagingConfiguration.prj')
